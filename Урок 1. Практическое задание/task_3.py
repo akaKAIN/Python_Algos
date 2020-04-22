fbb972a597b3cbb3fa763fcbc3959129ea9e990f
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2020-04-23 07:01:50 +1000

    task_3 wad done

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -12,4 +12,32 @@ k – угловой коэффициент (действительное чис
 Пример:
 X1_VAL = 2, Y1_VAL = 3, X2_VAL = 4, Y2_VAL = 5
 Уравнение прямой, проходящей через эти точки: y = 1.0x + 1.0
+
+k = (y1-y2) / (x1-x2)
+b = y2-k*x2
 """
+import sys
+
+POINT_X_A = 0
+POINT_Y_A = 0
+POINT_X_B = 0
+POINT_Y_B = 0
+K_VAL, B_VAL = 0, 0
+
+try:
+    POINT_X_A = int(input("Enter X-coordinate of point A: "))
+    POINT_Y_A = int(input("Enter Y-coordinate of point A: "))
+    POINT_X_B = int(input("Enter X-coordinate of point B: "))
+    POINT_Y_B = int(input("Enter Y-coordinate of point B: "))
+except ValueError:
+    print("Wrong coordinate of point")
+    sys.exit(1)
+
+try:
+    K_VAL = (POINT_X_A - POINT_X_B) / (POINT_Y_A - POINT_Y_B)
+except ZeroDivisionError:
+    print("Wrong coordinate of point")
+    sys.exit(1)
+
+B_VAL = POINT_Y_B - K_VAL * POINT_X_B
+print(f'equation: y = {K_VAL}x{B_VAL:+}')
