3ac8d65f1029e412b8f4412ec7b868a0c9509e7d
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2020-04-25 06:55:35 +1000

    Оптимизировано число строк ветвления

--- Урок 1. Практическое задание/task_9.py
+++ Урок 1. Практическое задание/task_9.py
@@ -4,22 +4,37 @@
 
 Подсказка: можно добавить проверку, что введены равные числа
 """
+import sys
 
-a = int(input("введите первое число: "))
-b = int(input("введите второе число: "))
-c = int(input("введите третье число: "))
 
-if a <= b:
-    if a <= c <= b:
-        print("среднее число: ", c)
-    elif c >= b:
-        print("среднее число: ", b)
-    else:
-        print("среднее число: ", a)
+try:
+    a = int(input("введите первое число: "))
+    b = int(input("введите второе число: "))
+    c = int(input("введите третье число: "))
+except ValueError:
+    print('Wrong numbers')
+    sys.exit(1)
+
+# if a <= b:
+#     if a <= c <= b:
+#         print("среднее число: ", c)
+#     elif c >= b:
+#         print("среднее число: ", b)
+#     else:
+#         print("среднее число: ", a)
+# else:
+#     if b <= c <= a:
+#         print("среднее число: ", c)
+#     elif c >= a:
+#         print("среднее число: ", a)
+#     else:
+#         print("среднее число: ", b)
+
+if b <= a <= c or b >= a >= c:
+    print(f'middle_num is {a}')
+elif a <= b <= c or a >= b >= c:
+    print(f'middle_num is {b}')
+elif a <= c <= b or a >= c >= b:
+    print(f'middle_num is {c}')
 else:
-    if b <= c <= a:
-        print("среднее число: ", c)
-    elif c >= a:
-        print("среднее число: ", a)
-    else:
-        print("среднее число: ", b)
+    print('something going wrong')
