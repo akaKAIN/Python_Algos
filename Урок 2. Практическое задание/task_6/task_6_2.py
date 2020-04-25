b59396772c9689def4a446df6a66ce79e8f2394e
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2020-04-25 17:44:37 +1000

    task_6 (recursion) was done

--- Урок 2. Практическое задание/task_6/task_6_2.py
+++ Урок 2. Практическое задание/task_6/task_6_2.py
@@ -7,3 +7,34 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+from random import randrange
+
+
+def guessing(secret_num: int, count: int):
+    count += 1
+    if count > 10:
+        print(f'Number {secret_num} was made up')
+        return
+    try:
+        user_num = int(input("Enter your number: "))
+        if user_num > secret_num:
+            print("Your number is greater")
+            guessing(secret_num, count)
+        elif user_num < secret_num:
+            print("Your number is less")
+            guessing(secret_num, count)
+        else:
+            print("You guessed!")
+            return
+    except ValueError:
+        print("Wrong number")
+        guessing(secret_num, count)
+
+
+def main():
+    secret_num = randrange(100)
+    guessing(secret_num, 0)
+
+
+if __name__ == '__main__':
+    main()
