72dbb7f519d1545c34b1bfc07109471c5e9eaa06
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2020-04-25 15:42:58 +1000

    task_6 was done

--- Урок 2. Практическое задание/task_6/task_6_1.py
+++ Урок 2. Практическое задание/task_6/task_6_1.py
@@ -7,3 +7,39 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+import random
+from sys import exit
+
+
+def input_user_number():
+    while True:
+        try:
+            return int(input("Enter number: "))
+        except ValueError:
+            continue
+
+
+def check_number(user_num, secret_num):
+    if user_num > secret_num:
+        return False, 'Your number is greater'
+    elif user_num < secret_num:
+        return False, 'Your number is less'
+    else:
+        return True, 'You guessed!'
+
+
+def main():
+    num = random.randrange(100)
+    count = 10
+    while count > 0:
+        user_num = input_user_number()
+        is_guessed, msg = check_number(user_num, num)
+        print(msg)
+        if is_guessed:
+            exit(0)
+        count -= 1
+    print(f'Number {num} was made up')
+
+
+if __name__ == '__main__':
+    main()
