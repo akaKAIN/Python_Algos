29cbc74ec9d0e12bea201ba7c5cbbc80ed80fce4
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2020-04-26 06:30:08 +1000

    task_7 was done

--- Урок 2. Практическое задание/task_7/task_7_1.py
+++ Урок 2. Практическое задание/task_7/task_7_1.py
@@ -5,3 +5,27 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+from random import randint
+
+
+def sum_simple_numbers(limit):
+    result = 0
+    for i in range(limit+1):
+        result += i
+    return float(result)
+
+
+def sum_by_expression(limit):
+    return limit * (limit + 1) / 2
+
+
+def main():
+    number = randint(0, 10 ** 8)
+    res_expr = sum_by_expression(number)
+    res_simple = sum_simple_numbers(number)
+    assert res_expr == res_simple, f"Something happened, and math laws was crash {res_expr} != {res_simple}"
+    print("All's fine")
+
+
+if __name__ == '__main__':
+    main()
