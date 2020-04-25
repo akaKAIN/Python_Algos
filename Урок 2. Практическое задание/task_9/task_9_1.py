117d2bade13ea93bccc5843a19e8092eda0e5e5a
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2020-04-26 07:18:10 +1000

    task_9 was done

--- Урок 2. Практическое задание/task_9/task_9_1.py
+++ Урок 2. Практическое задание/task_9/task_9_1.py
@@ -10,3 +10,37 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+
+
+def decimal_sum(num):
+    result = 0
+    while num > 0:
+        result += num % 10
+        num //= 10
+    return result
+
+
+def input_num(text):
+    while True:
+        try:
+            num = int(input(text))
+            return abs(num)
+        except ValueError:
+            print("Wrong number")
+
+
+def main():
+    max_sum, max_num = 0, 0
+    count = input_num("Enter count of numbers: ")
+    for i in range(count):
+        temp_num = input_num("Enter number: ")
+        temp_sum = decimal_sum(temp_num)
+        if temp_sum > max_sum:
+            max_sum = temp_sum
+            max_num = temp_num
+    print(f"Max sum of decimal numbers in {max_num}, sum of decimals is {max_sum}")
+
+
+if __name__ == '__main__':
+    main()
+
