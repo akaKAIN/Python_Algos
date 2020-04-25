cbc643ef7eb88bc3e0e8e8c9899fab8411961982
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2020-04-25 13:50:56 +1000

    task_2 was done.

--- Урок 2. Практическое задание/task_2/task_2_1.py
+++ Урок 2. Практическое задание/task_2/task_2_1.py
@@ -12,3 +12,40 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+from sys import exit
+
+
+def input_number() -> str:
+    while True:
+        try:
+            num = input("Enter number: ")
+            if int(num) <= 0:
+                print('Your number is not simple')
+                continue
+        except ValueError:
+            print("Wrong number")
+            continue
+        return num
+
+
+def print_num(text_number: str, num: str, multiplicity: int, is_multiplicity: bool):
+    print(f'{text_number}: ', end='')
+    for dec in num:
+        # Эта часть мне не нравится, но получше придумать не смог - код повторяется.
+        if is_multiplicity:
+            if int(dec) % multiplicity == 0:
+                print(dec, sep='', end=' ')
+        else:
+            if int(dec) % multiplicity != 0:
+                print(dec, sep='', end=' ')
+    print()
+
+
+def main():
+    num = input_number()
+    print_num('Even', num, 2, True)
+    print_num('Odd', num, 2, False)
+
+
+if __name__ == '__main__':
+    main()
--- Урок 2. Практическое задание/task_2/task_2_2.py
+++ Урок 2. Практическое задание/task_2/task_2_2.py
@@ -15,3 +15,64 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+from sys import exit
+
+
+def input_number():
+    try:
+        num = int(input("Enter number: "))
+        if num <= 0:
+            return input_number()
+    except ValueError:
+        print("Wrong number")
+        return input_number()
+    return num
+
+
+def math_operation_even(num):
+    global even_row
+    if num < 10:
+        even_row += str(num)
+        return num
+    unit = num % 10
+    if unit % 2 == 0:
+        even_row += str(unit)
+    num //= 10
+    return math_operation_even(num)
+
+
+def math_operation_odd(num):
+    global odd_row
+    if num < 10:
+        odd_row += str(num)
+        return num
+    unit = num % 10
+    if unit % 2 != 0:
+        odd_row += str(unit)
+    num //= 10
+    return math_operation_odd(num)
+
+
+def main():
+    num = input_number()
+    math_operation_even(num)
+    math_operation_odd(num)
+
+
+if __name__ == '__main__':
+    even_row = ''
+    odd_row = ''
+    main()
+    even_row = reversed(even_row)
+    odd_row = reversed(odd_row)
+
+    print('Even: ', sep='', end='')
+    for i in even_row:
+        print(i, end=' ')
+
+    print()
+    print('Odd: ', sep='', end='')
+    for i in odd_row:
+        print(i, end=' ')
+
+
