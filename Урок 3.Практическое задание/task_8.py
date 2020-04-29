8de9fa24fb9e7d028410fa0e68c56f2e7fc81c42
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2020-04-30 08:30:30 +1000

    Доделаны задания с 7 по 9

--- Урок 3.Практическое задание/task_8.py
+++ Урок 3.Практическое задание/task_8.py
@@ -36,3 +36,36 @@
 [3, 3, 3, 3, 12]
 [3, 3, 3, 3, 12]
 """
+import sys
+
+
+def input_matrix():
+    matrix = []
+    try:
+        for elem_matrix in range(5):
+            sum_input = 0
+            row_input = []
+            print(f'{elem_matrix + 1} row:')
+            for i in range(4):
+                num = int(input("Enter element of list: "))
+                row_input.append(num)
+                sum_input += num
+            row_input.append(sum(row_input))
+            matrix.append(row_input)
+        print_matrix(matrix)
+    except ValueError:
+        print("Wrong number.")
+        sys.exit(1)
+
+
+def print_matrix(matrix: list):
+    for row in matrix:
+        print(row)
+
+
+def main():
+    input_matrix()
+
+
+if __name__ == '__main__':
+    main()
--- Урок 3.Практическое задание/task_9.py
+++ Урок 3.Практическое задание/task_9.py
@@ -11,3 +11,37 @@
 [13, 12, 7, 15] минимальные значения по столбцам
 Максимальное среди них = 15
 """
+from random import randrange
+
+
+def generate_matrix():
+    count_elem = int(input("Enter count of elements in row: "))
+    count_row = int(input("Enter count of row in matrix: "))
+    matrix = []
+    for row in range(count_row):
+        temp_row = [randrange(100) for _ in range(count_elem)]
+        matrix.append(temp_row)
+    return matrix
+
+
+def get_min_column_elem(matrix: list):
+    min_list = []
+    count = len(matrix[0])
+    for i in range(count):
+        column = []
+        for row in matrix:
+            column.append(row[i])
+        min_list.append(min(column))
+    print(min_list, " min values in columns")
+    print(f'Max of them is {max(min_list)}')
+
+
+def main():
+    matrix = generate_matrix()
+    for i in matrix:
+        print(i)
+    get_min_column_elem(matrix)
+
+
+if __name__ == '__main__':
+    main()
