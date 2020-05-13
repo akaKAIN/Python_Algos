fecad955f14fdb450ca7a3c37db163409bc11f10
akaKAIN <mkain@mail.ru>
2020-05-14 05:41:41 +1000

    выполнены задания ДЗ №7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,3 +8,46 @@
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+import random
+
+
+def merge_and_sort(LIST):
+    if len(LIST) > 1:
+        cen = len(LIST) // 2
+        left = LIST[:cen]
+        rigth = LIST[cen:]
+
+        merge_and_sort(left)
+        merge_and_sort(rigth)
+
+        q = 0
+        w = 0
+        e = 0
+
+        while q < len(left) and w < len(rigth):
+            if left[q] < rigth[w]:
+                LIST[e] = left[q]
+                q += 1
+            else:
+                LIST[e] = rigth[w]
+                w += 1
+            e += 1
+
+        while q < len(left):
+            LIST[e] = left[q]
+            q += 1
+            e += 1
+
+        while w < len(rigth):
+            LIST[e] = rigth[w]
+            w += 1
+            e += 1
+
+
+if __name__ == '__main__':
+    NUMBER = int(input("Введите число элементов: "))
+    LIST = [random.random() * 50 for i in range(NUMBER)]
+
+    print(LIST)
+    merge_and_sort(LIST)
+    print(LIST)
